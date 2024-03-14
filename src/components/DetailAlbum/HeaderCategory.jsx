04b09e34916a4706271f5ca6c7a5244e3a857bf2
Yanis971 <yanisgeranhachon@gmail.com>
2024-03-14 11:09:09 +0100

    add category

---
+++ src/components/DetailAlbum/HeaderCategory.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const HeaderCategory = ({ dataAlbum }) => {
+    //petit element graphique pour faire un point
+    const Dot = () => (
+        <p className="mx-2">&#8226;</p>
+    )
+    //on récupère notre tableau de genre
+    const categories = dataAlbum?.genre
+
+    return (
+        <div className='flex items-center justify-start mt-4'>
+            {categories && categories.map((category, index) => (
+                //si c'est le premier élément on ne met pas de point
+                index === 0
+                    ? <Link key={index} to='#' className='font-medium '>{category.label}</Link>
+                    : <>
+                        <Dot />
+                        <Link key={index} to='#' className='font-medium '>{category.label}</Link>
+                    </>
+            ))}
+        </div>
+    )
+}
+
+export default HeaderCategory
